1384340a8a2bd3c282244e50a18ef1d8952702ae
hhaemin <ahrgoals@naver.com>
2024-07-18 11:58:28 +0900

    react-router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RootElement from "./components/RootElement";
+import Welcome from "./components/Welcome";
+import Products from "./components/Products";
+import { RouterProvider } from "react-router-dom";
+import ProductDetail from "./components/ProductDetail";
+import ErrorPage from "./components/ErrorPage";
+
+const router = BrowserRouter([
+  {
+    path: "/",
+    element: <RootElement />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/welcome",
+        element: <Welcome />,
+      },
+      {
+        path: "/products",
+        element: <Products />,
+      },
+      {
+        path: "/products/:productId",
+        element: <ProductDetail />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>dd</div>;
+  // return <RouterProvider router={router} />;
 }
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<RootElement />} />
+//         <Route path="/welcome" element={<Welcome />} />
+//         <Route path="/products" element={<Products />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
 
 export default App;
